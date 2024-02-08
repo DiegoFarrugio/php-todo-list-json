4d754f2f815ceb2315941bcd857eceeab3e7518a
DiegoFarrugio <diegofarrugio0@gmail.com>
2024-02-08 18:22:16 +0100

    Aggiunto bonus ma non funzionante

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -6,12 +6,5 @@ $thingsToDo = file_get_contents(__DIR__ . '/db.json');
 header('Content-Type: application/json');
 echo $thingsToDo;
 
-//BONUS (NON CORRETTO, DA AGGIUSTARE)
-//if($thingsToDo == false){
-//    echo 'No, già fatto';
-//}
-//else{
-//    echo $thingsToDo;
-//}
 
 ?>
